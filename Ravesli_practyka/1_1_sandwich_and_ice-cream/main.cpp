3d8146877812d03d8c42231160c488a871bb0ecc
21828 <nightcondor@ukr.net>
2019-07-24 12:58:07 +0300

    This programme surely works fine!

--- Ravesli_practyka/1_1_sandwich_and_ice-cream/main.cpp
+++ Ravesli_practyka/1_1_sandwich_and_ice-cream/main.cpp
@@ -5,36 +5,48 @@
 
 bool isSandwichWithIceCream (std :: string s)
 {
-    bool sandwich (false);
-    bool iceCream (false);
-    int check(0);
+//    bool sandwich (false);
+//    bool iceCream (false);
+
+    int lowerSandwichSize(0);
+    int upperSandwichSize(0);
+    int iceCreamSize(0);
+
+    char lowerSandwich(s[0]);
+    char upperSandwich(s[s.size() - 1]);
+    char iceCream (s[s.size() / 2]);
+
+    if (lowerSandwich != upperSandwich || lowerSandwich == iceCream || upperSandwich == iceCream )
+    {
+        return false;
+    }
 
     for (int i(0); i < s.size(); i++)
     {
-       if (s[i] == s[0])
-       {
-           if (!sandwich)
-           {
-               check++;
-               sandwich = true;
-           }
-           continue;
-       }
-
-       if (s[i] != s[0])
-       {
-           if (!iceCream)
-           {
-               iceCream = true;
-               sandwich = false;
-
-               check++;
-           }
-       }
-
+        if ( s[i] == lowerSandwich )
+        {
+            lowerSandwichSize++;
+        }
+        if( s[i] != lowerSandwich)
+        {
+            break;
+        }
     }
 
-    if ( sandwich  && iceCream  && check == 3)
+    for (int i(s.size() - 1) ; i != 0; i--)
+    {
+        if ( s[i] == lowerSandwich )
+        {
+            upperSandwichSize++;
+        }
+        if( s[i] != upperSandwich)
+        {
+            break;
+        }
+    }
+
+
+    if (upperSandwichSize == lowerSandwichSize)
     {
         return true;
     }
