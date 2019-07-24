d7a7228c67c7d10d648a17c32a7dbf442d6d2348
21828 <nightcondor@ukr.net>
2019-07-24 12:18:10 +0300

    Programme works perfectly!

--- Ravesli_practyka/1_1_sandwich_and_ice-cream/main.cpp
+++ Ravesli_practyka/1_1_sandwich_and_ice-cream/main.cpp
@@ -1,8 +1,55 @@
 #include <QCoreApplication>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
+#include <iostream>
+#include <string>
 
-    return a.exec();
+bool isSandwichWithIceCream (std :: string s)
+{
+    bool sandwich (false);
+    bool iceCream (false);
+    int check(0);
+
+    for (int i(0); i < s.size(); i++)
+    {
+       if (s[i] == s[0])
+       {
+           if (!sandwich)
+           {
+               check++;
+               sandwich = true;
+           }
+           continue;
+       }
+
+       if (s[i] != s[0])
+       {
+           if (!iceCream)
+           {
+               iceCream = true;
+               sandwich = false;
+
+               check++;
+           }
+       }
+
+    }
+
+    if ( sandwich  && iceCream  && check == 3)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+        std::string s;
+
+        std::cin >> s;
+
+        std::cout << isSandwichWithIceCream(s);
+        std::cout << '\n';
+
+        return 0;
 }
